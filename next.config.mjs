b5d736396277bf31bd508cd1fba28f1371750f43
km322 <mittal.ketan1@gmail.com>
2025-12-19 01:20:28 -0800

    Revert "try to optimize"
    This reverts commit 4f8e57c0b0adec5bbd5495aaaa0351db5e7d7e22.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // Removed unoptimized: true to enable image optimization
+  typescript: {
+    ignoreBuildErrors: true,
   },
-};
+  images: {
+    unoptimized: true,
+  },
+ 
+}
 
-export default nextConfig;
+export default nextConfig
